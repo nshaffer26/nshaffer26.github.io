466dd38c59ad0e8cce8025405802a8f014e54a03
nshaffer26 <nshaffer@buffalo.edu>
2016-12-16 00:47:16 -0500

    Initial Commit

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-function main() {
-  $('.hamburger').hide();
-  $('.menu').hide();
-  if($(window).width() <= 600){
-    $('.hamburger').show();
-    $('.buttons').hide();
-    $('.hamburger').on('click', function() {
-      //$(this).next().toggle();
-      $('.page').toggle();
-      $('.menu').toggle();
-    });
-  }
+$('.hamburger').hide();
+$('.menu').hide();
+if($(window).width() <= 600){
+  $('.hamburger').show();
+  $('.buttons').hide();
+  $('.hamburger').on('click', function() {
+    //$(this).next().toggle();
+    $('.page').toggle();
+    $('.menu').toggle();
+  });
 }
-$(document).ready(main);
