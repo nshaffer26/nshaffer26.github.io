fc0c1aaa73498ca0bd959b4a64e8623838691f0b
nshaffer26 <nshaffer@buffalo.edu>
2016-12-16 00:49:53 -0500

    Optimizations

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-$('.hamburger').hide();
-$('.menu').hide();
-if($(window).width() <= 600){
-  $('.hamburger').show();
-  $('.buttons').hide();
-  $('.hamburger').on('click', function() {
-    //$(this).next().toggle();
-    $('.page').toggle();
-    $('.menu').toggle();
-  });
+function main() {
+  $('.hamburger').hide();
+  $('.menu').hide();
+  if($(window).width() <= 600){
+    $('.hamburger').show();
+    $('.buttons').hide();
+    $('.hamburger').on('click', function() {
+      //$(this).next().toggle();
+      $('.page').toggle();
+      $('.menu').toggle();
+    });
+  }
 }
+main();
