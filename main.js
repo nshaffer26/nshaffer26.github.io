857ece5193331b18e67f8178450c5486d36b80f9
nshaffer26 <nshaffer@buffalo.edu>
2017-06-23 15:23:36 -0400

    Projects tab

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function main() {
   $('.menuMobile').hide();
   $('.dropdown').hide();
+  $('.moveToDesktop').hide();
   if($(window).width() <= 600){
+    $('.download').hide();
+    $('.moveToDesktop').show();
     $('.menu').hide();
     $('.menuMobile').show();
     $('.dropButton').on('click', function() {
