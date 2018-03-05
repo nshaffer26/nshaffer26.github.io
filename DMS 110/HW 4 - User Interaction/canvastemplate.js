c0a1ca64d5858e10a00b272285cf44a10e7b98e4
nshaffer26 <nshaffer@buffalo.edu>
2018-03-05 14:34:38 -0500

    Edited DMS 110 HW 4

--- DMS 110/HW 4 - User Interaction/canvastemplate.js
+++ DMS 110/HW 4 - User Interaction/canvastemplate.js
@@ -15,6 +15,8 @@ var rad;
 var mousePressed = false;
 var bErase,bBlack,bR,bY,bG,bB;
 
+//var undo = [];
+
 function setup() {
 	ctx.canvas.width  = window.innerWidth; 
 	ctx.canvas.height = window.innerHeight;
@@ -32,129 +34,149 @@ function draw() {
 
 	//Buttons
 	ctx.fillStyle = "rgb(255,255,255)";
-	ctx.fillRect(5,5,100,40);
+	ctx.fillRect(5,5,85,40);
+
+	//ctx.fillRect(95,5,85,40);
 	
 	if(rad == 35) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(110,5,100,40);
+		ctx.fillRect(95,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(110,5,100,40);
+		ctx.fillRect(95,5,85,40);
 	}
 	if(rad == 5) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(215,5,100,40);
+		ctx.fillRect(185,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(215,5,100,40);
+		ctx.fillRect(185,5,85,40);
 	}
 
 	if(bB) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(cv.width-105,5,100,40);
+		ctx.fillRect(cv.width-90,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(cv.width-105,5,100,40);
+		ctx.fillRect(cv.width-90,5,85,40);
 	}
 
 	if(bG) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(cv.width-210,5,100,40);
+		ctx.fillRect(cv.width-180,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(cv.width-210,5,100,40);
+		ctx.fillRect(cv.width-180,5,85,40);
 	}
 
 	if(bY) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(cv.width-315,5,100,40);
+		ctx.fillRect(cv.width-270,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(cv.width-315,5,100,40);
+		ctx.fillRect(cv.width-270,5,85,40);
 	}
 
 	if(bR) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(cv.width-420,5,100,40);
+		ctx.fillRect(cv.width-360,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(cv.width-420,5,100,40);
+		ctx.fillRect(cv.width-360,5,85,40);
 	}
 
 	if(bBlack) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(cv.width-525,5,100,40);
+		ctx.fillRect(cv.width-450,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(cv.width-525,5,100,40);
+		ctx.fillRect(cv.width-450,5,85,40);
 	}
 
 	if(bErase) {
 		ctx.fillStyle = "rgba(255,255,255,0.5)";
-		ctx.fillRect(cv.width-630,5,100,40);
+		ctx.fillRect(cv.width-540,5,85,40);
 	}
 	else {
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.fillRect(cv.width-630,5,100,40);
+		ctx.fillRect(cv.width-540,5,85,40);
 	}
 	
 	//Button Labels
 	ctx.fillStyle = "rgb(0,0,0)";
 	ctx.font = "30px Arial";
 	//Clear
-	ctx.fillText("Clear",10,35);
+	ctx.fillText("Clear",10,35,90);
 	//Erase
-	ctx.fillText("Erase",cv.width-620,35);
+	ctx.fillText("Erase",cv.width-535,35,75);
+	//Undo
+	//ctx.fillRect(122.5,24,30,2);
+	//ctx.moveTo(122.5,25);
+	//ctx.lineTo(130.5,15);
+	//ctx.lineTo(130.5,35);
 	//Increment
-	ctx.fillRect(145,24,30,2);
-	ctx.fillRect(159,10,2,30);
+	ctx.fillRect(122.5,24,30,2);
+	ctx.fillRect(136.5,10,2,30);
 	//Decrement
-	ctx.fillRect(250,24,30,2);
+	ctx.fillRect(212.5,24,30,2);
 	//Black Pen
 	ctx.fillStyle = "rgb(0,0,0)";
-	ctx.fillRect(cv.width-520,10,90,30);
+	ctx.fillRect(cv.width-445,10,75,30);
 	//Red Pen
 	ctx.fillStyle = "rgb(255,0,0)";
-	ctx.fillRect(cv.width-415,10,90,30);
+	ctx.fillRect(cv.width-355,10,75,30);
 	//Yellow Pen
 	ctx.fillStyle = "rgb(255,255,0)";
-	ctx.fillRect(cv.width-310,10,90,30);
+	ctx.fillRect(cv.width-265,10,75,30);
 	//Green Pen
 	ctx.fillStyle = "rgb(0,255,0)";
-	ctx.fillRect(cv.width-205,10,90,30);
+	ctx.fillRect(cv.width-175,10,75,30);
 	//Blue Pen
 	ctx.fillStyle = "rgb(0,0,255)";
-	ctx.fillRect(cv.width-100,10,90,30);
+	ctx.fillRect(cv.width-85,10,75,30);
 }
 
 /*Event Functions*/
 function mouseDown() {
 	mousePressed = true;
+	//undo[0] = rad;
 	if(mouseY < 50 && mouseY > 5 && mouseY < 45) {
-		if(mouseX > 5 && mouseX < 105) {
-			//Erase
+		if(mouseX > 5 && mouseX < 90) {
+			//Clear
 			clear();
 		}
-		else if(mouseX > 110 && mouseX < 210) {
+		/*else if(mouseX > 95 && mouseX < 180) {
+			//Undo
+			for(var i = 1; i < undo.length; i+=2) {
+				var ra = undo[0];
+
+				ctx.fillStyle = "rgb(255,255,255)";
+				ctx.beginPath();
+				ctx.arc(undo[i],undo[i+1],ra+1,0,Math.PI*2);
+				ctx.fill();
+			}
+			undo = [];
+		}*/
+		else if(mouseX > 95 && mouseX < 180) {
 			//Increment Pen Size
 			if(rad >= 5 && rad < 35) {
 				rad = rad + 5;
 			}
 		}
-		else if(mouseX > 215 && mouseX < 315) {
+		else if(mouseX > 185 && mouseX < 270) {
 			//Decrement Pen Size
 			if(rad > 5 && rad <= 35) {
 				rad = rad - 5;
 			}
 		}
-		else if(mouseX > cv.width-105 && mouseX < cv.width-125 + 100) {
+		else if(mouseX > cv.width-90 && mouseX < cv.width-90 + 85) {
 			//Blue Pen
 			bErase = bBlack = bR = bY = bG = false;
 			bB = true;
@@ -162,7 +184,7 @@ function mouseDown() {
 			g = 0;
 			b = 255;
 		}
-		else if(mouseX > cv.width-210 && mouseX < cv.width-210 + 100) {
+		else if(mouseX > cv.width-180 && mouseX < cv.width-180 + 85) {
 			//Green Pen
 			bErase = bBlack = bR = bY = bB = false;
 			bG = true;
@@ -170,7 +192,7 @@ function mouseDown() {
 			g = 255;
 			b = 0;
 		}
-		else if(mouseX > cv.width-315 && mouseX < cv.width-315 + 100) {
+		else if(mouseX > cv.width-270 && mouseX < cv.width-270 + 85) {
 			//Yellow Pen
 			bErase = bBlack = bR = bG = bB = false;
 			bY = true;
@@ -178,7 +200,7 @@ function mouseDown() {
 			g = 255;
 			b = 0;
 		}
-		else if(mouseX > cv.width-420 && mouseX < cv.width-420 + 100) {
+		else if(mouseX > cv.width-360 && mouseX < cv.width-360 + 85) {
 			//Red Pen
 			bErase = bBlack = bY = bG = bB = false;
 			bR = true;
@@ -186,7 +208,7 @@ function mouseDown() {
 			g = 0;
 			b = 0;
 		}
-		else if(mouseX > cv.width-525 && mouseX < cv.width-525 + 100) {
+		else if(mouseX > cv.width-450 && mouseX < cv.width-450 + 85) {
 			//Black Pen
 			bErase = bR = bY = bG = bB = false;
 			bBlack = true;
@@ -194,7 +216,7 @@ function mouseDown() {
 			g = 0;
 			b = 0;
 		}
-		else if(mouseX > cv.width-630 && mouseX < cv.width-630 + 100) {
+		else if(mouseX > cv.width-540 && mouseX < cv.width-540 + 85) {
 			//Eraser
 			bBlack = bR = bY = bG = bB = false;
 			bErase = true;
@@ -204,6 +226,10 @@ function mouseDown() {
 		}
 	}
 	else {
+		//undo = [];
+		//undo[0] = rad;
+		//undo[undo.length] = mouseX;
+		//undo[undo.length] = mouseY;
 		color = "rgb(" + r + "," + g + "," + b + ")";
 		ctx.fillStyle = color;
 		ctx.beginPath();
@@ -225,6 +251,8 @@ function mouseMove(e) {
 		ctx.beginPath();
 		ctx.arc(mouseX,mouseY,rad,0,Math.PI*2);
 		ctx.fill();
+		//undo[undo.length] = mouseX;
+		//undo[undo.length] = mouseY;
 	}
 }
 
@@ -244,4 +272,4 @@ canvas.addEventListener('mouseup', mouseUp);
 canvas.addEventListener('mouseleave', mouseOut);
 //canvas.addEventListener('mouseleave', mouseUp);
 setup();
-setInterval(draw, 10);
+setInterval(draw, 5);
